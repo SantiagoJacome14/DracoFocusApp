077a4da7c135e83339011672b9224e3a44e579ec
Santiago Jacome <sjacome249@unab.edu.co>
2026-04-29 15:52:57 -0500

    feat/ux: rediseño del Gran Museo con StaggeredGrid y sistema visual de bloqueo

--- app/src/main/java/co/edu/unab/dracofocusapp/ui/MuseoDracoArteScreen.kt
+++ app/src/main/java/co/edu/unab/dracofocusapp/ui/MuseoDracoArteScreen.kt
@@ -1,5 +1,6 @@
 package co.edu.unab.dracofocusapp.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
